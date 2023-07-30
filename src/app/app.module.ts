6a892d81219db2c482d0c62e2689116c48609cb2
danifoj315 <danifoj315@student.polsl.pl>
2023-07-30 16:06:13 +0200

    completed Cart Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HomeComponent } from "./pages/home/home.component";
 import { ProductsHeaderComponent } from "./pages/home/components/products-header/products-header.component";
 import { FiltersComponent } from './pages/home/components/filters/filters.component';
 import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { ProductBoxComponent } from './pages/home/components/product-box/product
     ProductsHeaderComponent,
     FiltersComponent,
     ProductBoxComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
